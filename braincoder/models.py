40ae4e294eb24283c7704017458589ead1fa5fa3
mdaghlian <marcusdag@googlemail.com>
2025-10-10 11:37:03 +0200

    actually fixed the typo - which was fine

--- braincoder/models.py
+++ braincoder/models.py
@@ -2028,12 +2028,12 @@ class ContrastSensitivity(EncodingModel):
         # Combine the curves using boolean masks
         
         # # Split stimulus space into left and right
-        # is_left = log_SF_seq > log_SFp
+        # is_left = log_SF_seq < log_SFp
         # csf = tf.where(is_left, L_curve, R_curve) # REMOVE THE "WHERE" - makes unstable gradients
         # Smooth transition instead of hard `tf.where`
-        alpha = 50.0  # Adjust to control smoothness
+        alpha = 500.0  # Adjust to control smoothness
         blend_factor = tf.math.sigmoid(alpha * (log_SF_seq - log_SFp))
-        csf = blend_factor * R_curve + (1 - blend_factor) * L_curve  
+        csf = (1 - blend_factor) * L_curve + blend_factor * R_curve 
               
         return csf 
 
